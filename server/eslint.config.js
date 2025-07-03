c92df6c96cc022f98fcf073a7e0231efd86a33b8
Carlos <carlos.a.dosreis@gmail.com>
2025-07-03 14:57:44 -0300

    feat: adiciona o backend do projeto

---
+++ server/eslint.config.js
@@ -0,0 +1,37 @@
+import globals from 'globals';
+import pluginJs from '@eslint/js';
+import pluginPrettier from 'eslint-plugin-prettier';
+import configPrettier from 'eslint-config-prettier';
+
+export default [
+  {
+    ignores: ['node_modules/', 'dist/'],
+  },
+  pluginJs.configs.recommended,
+  {
+    languageOptions: {
+      ecmaVersion: 'latest',
+      sourceType: 'module',
+      globals: {
+        ...globals.node,
+      },
+    },
+    rules: {
+      'no-console': 'warn',
+      'no-unused-vars': [
+        'warn',
+        { vars: 'all', args: 'after-used', ignoreRestSiblings: true },
+      ],
+      'prefer-const': 'error',
+    },
+  },
+  {
+    plugins: {
+      prettier: pluginPrettier,
+    },
+    rules: {
+      ...configPrettier.rules,
+      'prettier/prettier': 'error',
+    },
+  },
+];
