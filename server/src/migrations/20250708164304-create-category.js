ecbc7aaeaee207df2293f1f4249c8c4ee7a83d60
Carlos <carlos.a.dosreis@gmail.com>
2025-07-08 14:04:50 -0300

    feat: adiociona a tabela categories ao BD com sequelize

---
+++ server/src/migrations/20250708164304-create-category.js
@@ -0,0 +1,47 @@
+'use strict';
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('Categories', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER,
+      },
+      name: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      icon: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      color: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      user_id: {
+        type: Sequelize.INTEGER,
+        allowNull: true,
+        references: {
+          model: 'Users',
+          key: 'id',
+        },
+        onDelete: 'CASCADE',
+        onUpdate: 'CASCADE',
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+      },
+    });
+  },
+  async down(queryInterface, _Sequelize) {
+    await queryInterface.dropTable('Categories');
+  },
+};
---
+++ server/src/models/category.js
@@ -0,0 +1,48 @@
+'use strict';
+const { Model } = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class Category extends Model {
+    static associate(models) {
+      Category.hasMany(models.Limit, {
+        foreignKey: 'category_id',
+        as: 'limits',
+        onDelete: 'SET NULL',
+        onUpdate: 'CASCADE',
+      });
+
+      Category.hasMany(models.Transaction, {
+        foreignKey: 'category_id',
+        as: 'transactions',
+        onDelete: 'SET NULL',
+        onUpdate: 'CASCADE',
+      });
+
+      Category.belongsTo(models.User, {
+        foreignKey: 'user_id',
+        as: 'user',
+      });
+    }
+  }
+  Category.init(
+    {
+      name: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+      icon: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+      color: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+    },
+    {
+      sequelize,
+      modelName: 'Category',
+      tableName: 'Categories',
+    },
+  );
+  return Category;
+};
